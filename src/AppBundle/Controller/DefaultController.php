0c7a86c01c315ee634a2e6bd5ac212e34a3a1b82
vova <vova@vova-X553MA>
2017-02-01 09:30:46 +0200

    new branch

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,7 +13,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        dump($request);        die();
+       // dump($request);   gg      die();
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
